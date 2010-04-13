8f61329ee56a0ddcf064121944888a1219c093de
Martin Morgan <mtmorgan@fhcrc.org>
2010-04-13 19:05:06 +0000

    track XML 2.8-1
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AffyCompatible@45957 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 xmlValue.XMLAttributeValue <-
-    function(x, ignoreComments=FALSE, recursive=TRUE)
+    function(x, ignoreComments=FALSE, recursive=TRUE, encoding=XML:::CE_NATIVE)
 {
     as.character(x)
 }
