eee54df18cd4d0be0b747d369da7436fd602d79c
lshep <lori.shepherd@roswellpark.org>
2022-05-10 11:38:58 -0400

    user requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.17")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
 xmlValue.XMLAttributeValue <-
 
     function(x, ignoreComments=FALSE, recursive=TRUE,
